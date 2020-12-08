e2d19b1c064a1fe9e197da1d85c77aaa12caf935
Guilherme Henrique <guiihdourado@gmail.com>
2020-12-08 19:41:30 -0300

    @web - Add Page Structure

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,42 +1,61 @@
 module.exports = {
   "env": {
+    "browser": true,
     "es2020": true,
-    "node": true
+    "node": true,
+    "jest": true,
   },
   "extends": [
+    "plugin:react/recommended",
     "airbnb-base",
     "plugin:@typescript-eslint/recommended",
     "prettier/@typescript-eslint",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
+    "prettier/react"
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
     "ecmaVersion": 2018,
     "sourceType": "module"
   },
   "plugins": [
+    'react',
     "@typescript-eslint",
-    "prettier"
+    "prettier",
+    "react-hooks"
   ],
   "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "no-use-before-define": "off",
     "prettier/prettier": "error",
     "no-underscore-dangle": "off",
+    "no-unused-expressions": "off",
     "class-methods-use-this": "off",
     "camelcase": "off",
     "@typescript-eslint/no-unused-vars": ["error", {
       "argsIgnorePattern": "_"
     }],
+    "import/prefer-default-export": "off",
     "import/extensions": [
       "error",
       "ignorePackages",
       {
-        "ts": "never"
+        "ts": "never",
+        "tsx": "never"
       }
     ],
   },
   "settings": {
     "import/resolver": {
       "typescript": {}
+    },
+    "react": {
+      "version": "detect"
     }
   }
 }
