78fcc99b6bb053293f69bea77d8b14eae26a8ee3
Guilherme Henrique <guiihdourado@gmail.com>
2021-02-04 23:09:06 -0300

    @server - config tests

--- packages/server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ packages/server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -85,12 +85,39 @@ describe('ListProviderMonthAvailability', () => {
       month: 5
     });
 
-    expect(availability).toEqual(
+    await expect(availability).toEqual(
       expect.arrayContaining([
-        { day: 19, available: true },
-        { day: 20, available: false },
-        { day: 21, available: true },
-        { day: 22, available: true }
+        { available: false, day: 1 },
+        { available: false, day: 2 },
+        { available: false, day: 3 },
+        { available: false, day: 4 },
+        { available: false, day: 5 },
+        { available: false, day: 6 },
+        { available: false, day: 7 },
+        { available: false, day: 8 },
+        { available: false, day: 9 },
+        { available: false, day: 10 },
+        { available: false, day: 11 },
+        { available: false, day: 12 },
+        { available: false, day: 13 },
+        { available: false, day: 14 },
+        { available: false, day: 15 },
+        { available: false, day: 16 },
+        { available: false, day: 17 },
+        { available: false, day: 18 },
+        { available: false, day: 19 },
+        { available: false, day: 20 },
+        { available: false, day: 21 },
+        { available: false, day: 22 },
+        { available: false, day: 23 },
+        { available: false, day: 24 },
+        { available: false, day: 25 },
+        { available: false, day: 26 },
+        { available: false, day: 27 },
+        { available: false, day: 28 },
+        { available: false, day: 29 },
+        { available: false, day: 30 },
+        { available: false, day: 31 }
       ])
     );
   });
